24a058b27ec276d8ed8ea0c2809fdd63dca6d843
Tony Pujals <tpujals@gmail.com>
2023-11-29 03:48:04 -0800

    minor

--- express-tsx-jest/types/app.d.ts
+++ express-tsx-jest/types/app.d.ts
@@ -1,12 +1,5 @@
 /// <reference types="node" resolution-mode="require"/>
 import { Server } from "node:http";
-declare global {
-    namespace Express {
-        interface Request {
-            user?: any;
-        }
-    }
-}
 export declare function start(port: number | string): Promise<Server>;
 export declare function stop(signal?: string | Error): Promise<void>;
 //# sourceMappingURL=app.d.ts.map
