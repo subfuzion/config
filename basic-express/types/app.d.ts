8ec93cfc6bb4f6fdca794832ae9c8ca8c8e3ba63
Tony Pujals <tpujals@gmail.com>
2023-12-01 05:40:03 -0800

    restructure

--- basic-express/types/app.d.ts
+++ basic-express/types/app.d.ts
@@ -1,2 +1,5 @@
-export {};
+/// <reference types="node" resolution-mode="require"/>
+import { Server } from "node:http";
+export declare function start(port: number | string): Promise<Server>;
+export declare function stop(signal?: string | Error): Promise<void>;
 //# sourceMappingURL=app.d.ts.map
--- express-tsx-jest/types/app.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="node" resolution-mode="require"/>
-import { Server } from "node:http";
-export declare function start(port: number | string): Promise<Server>;
-export declare function stop(signal?: string | Error): Promise<void>;
-//# sourceMappingURL=app.d.ts.map
